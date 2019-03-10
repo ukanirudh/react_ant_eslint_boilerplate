4d6f2184cd74505616f07327890ec58421c72e28
U K ANIRUDH <anirudh.anirudh93@gmail.com>
2019-03-10 12:30:46 +0530

    Anirudh : Adding webpack config for fonts and images

--- webpack.config.js
+++ webpack.config.js
@@ -50,6 +50,26 @@ module.exports = {
           }
         ],
       },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [{
+            loader: 'file-loader',
+            options: {
+                name: '[name].[ext]',
+                outputPath: 'fonts/'
+            }
+        }]
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [{
+            loader: 'file-loader',
+            options: {
+                name: '[name].[ext]',
+                outputPath: 'images/'
+            }
+        }]
+      },
     ]
   },
   plugins: [
